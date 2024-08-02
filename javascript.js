9b4d869cd6552e38789d45e8a73052cddce11a86
bankeytech <122796865+bankeytech@users.noreply.github.com>
2024-08-02 03:27:25 +0100

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,28 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const navLinks = document.querySelectorAll('ul a');
 
+  navLinks.forEach(link => {
+      link.addEventListener('click', function(event) {
+          event.preventDefault();
+          const targetId = this.getAttribute('href').substring(1);
+          const targetSection = document.getElementById(targetId);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+          window.scrollTo({
+              top: targetSection.offsetTop - 50, // Adjust this value according to your fixed nav height
+              behavior: 'smooth'
+          });
+      });
+  });
+});
 
 
 
